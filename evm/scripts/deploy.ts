bdf5daae8cbbd666868674e08da95fcc3b0bb016
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-09 15:38:46 -0500

    evm: deploy scripts

---
+++ evm/scripts/deploy.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { ethers } from "hardhat";
+import recordDeployment from "./record-deployment";
+
+const deployContract = async (contractName: string) => {
+  const ContractFactory = await ethers.getContractFactory(contractName);
+  const contract = await ContractFactory.deploy();
+
+  await contract.deployed();
+
+  const deploymentRecord = await recordDeployment(contractName, contract);
+  console.log(`${contractName} deployed:`, deploymentRecord);
+};
+
+async function main() {
+  // set the contract name(s) in here
+  // NOTE: must match the <name>.sol file, ex: for Greeter.sol contractName = "Greeter"
+  const contractNames = [""];
+
+  await Promise.all(contractNames.map(deployContract));
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
