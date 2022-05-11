703f4c1bb05164dbe17cc871045effa42d433d7c
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-11 12:49:26 -0500

    evm/test: utils and constants

---
+++ evm/test/constants.ts
@@ -0,0 +1,16 @@
+// source: https://gist.github.com/the-vampiire/1b4e8cd92b20dc4f034457c926210965
+
+export const INTERFACE_IDS = {
+  ERC165: "0x01ffc9a7",
+  ERC721: "0x80ac58cd",
+  ERC721Enumerable: "0x780e9d63",
+  ERC721Metadata: "0x5b5e139f",
+  ERC1155: "0xd9b67a26",
+  ERC1155Receiver: "0x4e2312e0",
+  AccessControl: "0x7965db0b",
+  AccessControlEnumerable: "0x5a05180f",
+  Governor: "0xbf26d897",
+  GovernorWithParams: "0x79dd796f",
+  GovernorTimelock: "0x6e665ced",
+  ERC2981: "0x2a55205a",
+};
---
+++ evm/test/utils.ts
@@ -0,0 +1,69 @@
+import { ethers } from "hardhat";
+
+import type { Signer, Contract } from "ethers";
+import type { NFNovel, NFNovels } from "../typechain";
+
+/**
+ * Deploys a contract and returns an instance of it for tests
+ * @param ownerAccount the signer that deploys the contract
+ * @param constructorArgs optional args to pass to .deploy()
+ */
+const deployTestContract = async <TContract extends Contract>(
+  contractName: string,
+  ownerAccount: Signer,
+  ...constructorArgs: unknown[]
+): Promise<TContract> => {
+  const ContractFactory = await ethers.getContractFactory(contractName);
+
+  const contract = await ContractFactory.connect(ownerAccount).deploy(
+    ...constructorArgs
+  );
+
+  await contract.deployed();
+
+  return contract as TContract;
+};
+
+export const deployNFNovelTestContract = (
+  ownerAccount: Signer,
+  ...constructorArgs: unknown[]
+): Promise<NFNovel> =>
+  deployTestContract<NFNovel>("NFNovel", ownerAccount, ...constructorArgs);
+
+export const deployNFNovelsTestContract = (
+  ownerAccount: Signer,
+  ...constructorArgs: unknown[]
+): Promise<NFNovels> =>
+  deployTestContract<NFNovels>("NFNovels", ownerAccount, ...constructorArgs);
+
+/**
+ * Destructure as many accounts as needed for tests
+ * @note the first signer is the default signing account (if none is specified when deploying or calling a contract)
+ *
+ * For just signers:
+ * @example
+ * ```js
+ * [[firstSigner], [secondSigner], ...] = await getTestSigningAccounts();
+ * ```
+ *
+ * For signers and their addresses
+ * @example
+ * ```js
+ * [
+ *  [firstSigner, firstSignerAddress],
+ *  [secondSigner, secondSignerAddress],
+ *  ...
+ * ] = await getTestSigningAccounts();
+ * ```
+ *
+ * @returns signer and address pairs
+ */
+export const getTestSigningAccounts: () => Promise<
+  [Signer, string][]
+> = async () => {
+  const signers = await ethers.getSigners();
+
+  return Promise.all(
+    signers.map(async (signer) => [signer, await signer.getAddress()])
+  );
+};
