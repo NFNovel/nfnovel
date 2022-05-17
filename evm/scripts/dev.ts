2f7f398d40af78f44664d264ff9bfb20abca8adb
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-17 08:04:15 -0500

    evm: dev setup script (deploy, add-page)
    Co-authored-by: Aman Adhav <amanadhav@gmail.com>
    Co-authored-by: Fede Sarquis <fede2442@users.noreply.github.com>

---
+++ evm/scripts/dev.ts
@@ -0,0 +1,18 @@
+import { deployContracts } from "./deploy";
+import { addPages } from "./add-pages";
+
+async function devSetup(): Promise<void> {
+  /**
+   * 1. deploy the contrac(s)
+   * 2. add the pages
+   */
+  const deployments = await deployContracts();
+  await addPages(deployments.NFNovel.contractAddress);
+}
+
+devSetup().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+export default devSetup;
