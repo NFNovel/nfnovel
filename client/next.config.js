10c84526012ccd0544e52c0f90ed02b2d651ce3a
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-18 22:25:46 -0500

    [WHY] client: disable react strict (conflict w IPFS...)

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // NOTE: fix for LockExistsError: Lock already being held for file: ipfs/repo.lock
+  // turn this to false
+  // i dont get it but it causes everything to load twice which triggers an issue with IPFS repo lock...
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
