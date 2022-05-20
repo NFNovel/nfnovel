8b5228d3de88bf41962ced975cdb87accfdb71ce
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-20 11:10:59 -0500

    client: Page integrate with Panel component

--- client/src/components/Page.tsx
+++ client/src/components/Page.tsx
@@ -4,6 +4,8 @@ import { useContext, useEffect, useState } from "react";
 import { NFNovelContext } from "src/contexts/nfnovel-context";
 import { PanelData, PanelContext } from "src/contexts/panel-context";
 
+import Panel from "./Panel";
+
 import type { Page } from "src/types/page";
 
 function Page(props: { pageData: Page }) {
@@ -38,8 +40,6 @@ function Page(props: { pageData: Page }) {
     null,
   );
 
-  // NOTE: this should be on all pages...site is useless without these two
-
   useEffect(() => {
     // define a function that will call getPagePanelsData
     async function loadPanelsData() {
@@ -56,8 +56,6 @@ function Page(props: { pageData: Page }) {
   }, [pageData, panelContext]);
 
   if (!nfnovel || !panelContext || !pagePanelsData) return <Spinner />;
-  // we want to load the pagePanelsData when the component mounts
-  // we will do this calling getPagePanelsData
 
   return (
     <section className="mt-12 relative overflow-hidden py-12 px-4 border border-gray-900 sm:px-8">
@@ -68,20 +66,10 @@ function Page(props: { pageData: Page }) {
       </div>
       <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
         {pagePanelsData.map((panelData, key) => (
-          // all of this can be the basis for the Panel component
-          <article
-            className="max-w-md mx-auto mt-4 bg-blue-800 shadow-lg border rounded-md duration-300 hover:shadow-sm"
-            key={key}
-          >
-            <div className="filter opacity-100 hover:opacity-50 hover:red-500 duration-1000">
-              {panelData.imageSource && (
-                <img
-                  src={panelData.imageSource}
-                  className="w-full h-48 rounded-t-md"
-                />
-              )}
-            </div>
-          </article>
+          <Panel
+            panelData={panelData}
+            key={`page-${pageData.pageNumber.toNumber()}-panel-${key}`}
+          />
         ))}
       </div>
       <div className={"flex justify-center mt-10 text-3xl"}>
