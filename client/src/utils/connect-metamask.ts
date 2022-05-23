cb914047806651eb948aebab1964e2483ded1c60
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-23 16:35:11 -0500

    client/utils: connect to metamask check is isPanelOwner

--- client/src/utils/connect-metamask.ts
+++ client/src/utils/connect-metamask.ts
@@ -1,3 +1,5 @@
+import PanelOwnerService from "src/services/panel-owner-service";
+
 import type { Web3Provider } from "@ethersproject/providers";
 import type { IConnectedAccount } from "src/contexts/nfnovel-context";
 
@@ -9,8 +11,19 @@ export const connectToMetamask = async (
 
     const signer = await metamaskProvider.getSigner();
     const address = await signer.getAddress();
+    const ownedPanelTokenIds = await PanelOwnerService.getOwnedPanelTokenIds(
+      address,
+    );
 
-    return { signer, address };
+    const connectedAccount: IConnectedAccount = {
+      signer,
+      address,
+      isPanelOwner: false,
+    };
+
+    if (ownedPanelTokenIds.length) connectedAccount.isPanelOwner = true;
+
+    return connectedAccount;
   } catch {
     return null;
   }
