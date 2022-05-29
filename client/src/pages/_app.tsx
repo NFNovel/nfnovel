f401ec3ad27a62c5b23f00eea3ae0d3822d5f63a
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-05-28 20:00:50 -0500

    [WIP] client: configure WAGMI provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,17 +1,49 @@
 import "@styles/globals.css";
 
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import WithNFNovel from "src/contexts/nfnovel-context";
 import WithPanelData from "src/contexts/panel-context";
+
+const {
+  chains,
+  provider,
+  webSocketProvider
+} = configureChains(
+  [chain.mainnet, chain.goerli, chain.hardhat],
+  [
+    jsonRpcProvider({
+      rpc: (rpcChain) => {
+        // TODO: how to handle mainnet and goerli testnet?
+
+        const localRpcHost = "localhost:8545";
+
+        return {
+          http: `http://${localRpcHost}`,
+          webSocket: `ws://${localRpcHost}`,
+        };
+      },
+    }),
+  ],
+);
+
+const wagmiClient = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: false,
+});
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WithNFNovel>
-      <WithPanelData>
-        <Component {...pageProps} />;
-      </WithPanelData>
-    </WithNFNovel>
+    <WagmiConfig client={wagmiClient}>
+      <WithNFNovel>
+        <WithPanelData>
+          <Component {...pageProps} />;
+        </WithPanelData>
+      </WithNFNovel>
+    </WagmiConfig>
   );
 }
 
