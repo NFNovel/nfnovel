7088a4368c0b1c6cbe8af66d487e37c66bbfcace
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-06-09 18:11:55 -0500

    client: homepage notes (future, platform)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,24 +1,52 @@
-import styles from "@styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { Spinner } from "@chakra-ui/react";
 
 import Novel from "src/components/Novel";
-
-import Header from "../components/Header";
+import useNFNovel from "src/hooks/use-nfnovel";
 
 import type { NextPage } from "next";
 
+type NovelDetails = {
+  title: string;
+  tokenSymbol: string;
+};
+
+/**
+ * THINK: routing based on novel ID (from NFNovels factory)
+ * event NovelCreated(uint256 indexed novelId, address indexed novelContract);
+ * /<novelId> --> search for NovelCreated filter novelId --> contractAddress
+ *
+ * ref useNFNovel() --> useNFNovel(contractAddress) --> load instance for that address
+ *
+ * create NFNovels model to manage fetching/caching
+ *
+ * home page becomes selection of novels, upcoming votes/pledging, artists etc
+ *
+ */
+
 const Home: NextPage = () => {
+  const { getNovelDetails } = useNFNovel();
+
+  const [novelDetails, setNovelDetails] = useState<NovelDetails | undefined>();
+
+  useEffect(() => {
+    const loadNovelDetails = async () => {
+      const novelDetails = await getNovelDetails();
+      setNovelDetails(novelDetails);
+    };
+
+    if (!novelDetails) loadNovelDetails();
+  }, [novelDetails, getNovelDetails]);
+
+  if (!novelDetails) return <Spinner />;
+
+  const { title, tokenSymbol } = novelDetails;
+
   return (
-    <>
-      <Header />
-      <Novel
-        summary={
-          "Summary of the novel asidjfoasijdfo aijsdp ofijaspodifj pasodijf paosijdf poaisjdfp oiasjdfpoija sdpfoijas podifjspaodijf pasoijdf poaisjdfp oiasjdfpoi jaspdoifj aspodifj apsoidjf"
-        }
-        title={"Some title for the novel"}
-        author={"random_auth"}
-        id={1}
-      />
-    </>
+    <Novel
+      title={title}
+      id={1}
+    />
   );
 };
 
