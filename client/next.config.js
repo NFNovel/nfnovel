63702d6f4320ecb21f42916f5b6b58a2f05868ba
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-06-27 15:15:50 -0500

    client: ref to use copied EVM artifacts

--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   // turn this to false
   // i dont get it but it causes everything to load twice which triggers an issue with IPFS repo lock...
   reactStrictMode: false,
-  experimental: {
-    externalDir: true,
-  }
 }
 
 module.exports = nextConfig
